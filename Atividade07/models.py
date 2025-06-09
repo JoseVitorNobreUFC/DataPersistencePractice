6d8aa04c70b6d35407edaba96a1bedde2e579264
JoseVitorNobreUFC <165806736+JoseVitorNobreUFC@users.noreply.github.com>
2025-06-09 00:30:33 +0000

    finalizei a atividade

--- Atividade07/models.py
+++ Atividade07/models.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, String
 class Autor(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     nome: str
+    email: Optional[str] = Field(default=None, index=True)
     email: Optional[str] = Field(
         default=None,
         sa_column=Column(String, index=True)  # GARANTE index e tipo correto!
