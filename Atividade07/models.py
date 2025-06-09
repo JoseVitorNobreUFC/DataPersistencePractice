56bd786e63f65f4eda5377fc4450ed9fe9dc412c
JoseVitorNobreUFC <165806736+JoseVitorNobreUFC@users.noreply.github.com>
2025-06-09 00:47:07 +0000

    finalizei a atv 7

--- Atividade07/models.py
+++ Atividade07/models.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, String
 class Autor(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     nome: str
-    email: Optional[str] = Field(default=None, index=True)
+    cpf: Optional[str] = Field(default=None, sa_column=Column(String)) # Removi unique pois dá erro no SQLite
     email: Optional[str] = Field(
         default=None,
         sa_column=Column(String, index=True)  # GARANTE index e tipo correto!
